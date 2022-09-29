669c61d1513aa1fe744d4fb8b95de14895f0c257
Aleksei Lesnik <lesnik-a-a@yandex.ru>
2022-09-29 23:57:39 +0300

    Added more code.

--- server/db/user_db_api.py
+++ server/db/user_db_api.py
@@ -1,4 +1,5 @@
 from server.common.abstract import User
+from typing import Optional
 from server.db.user_db_impl import __add_new_user, __get_user_by_login
 
 
@@ -6,6 +7,6 @@ def add_new_user(user: User):
     return __add_new_user(user)
 
 
-def get_user_by_login(login: str) -> User:
+def get_user_by_login(login: str) -> Optional[User]:
     return __get_user_by_login(login)
 
