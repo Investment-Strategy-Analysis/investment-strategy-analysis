1e21aefe76189cb37851d80b9980d5bc247c0504
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-10-07 00:34:43 +0300

    Add login and signup pages

--- services/ui_service/vite.config.js
+++ services/ui_service/vite.config.js
@@ -1,12 +1,26 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+const root = resolve(__dirname, 'src');
+const outDir = resolve(__dirname, 'dist');
+
 export default defineConfig({
+  root: root,
   plugins: [solidPlugin()],
   server: {
     port: 3000,
   },
   build: {
+    outDir: outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        login: resolve(root, 'login', 'index.html'),
+        signup: resolve(root, 'signup', 'index.html'),
+      }
+    },
     target: 'esnext',
   },
 });
