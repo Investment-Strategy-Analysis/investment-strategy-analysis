95137904218f200a4332e0ea71f0dbf1d74d3a20
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-10-31 00:19:03 +0300

    Update graphic

--- services/ui_service/src/components/GraphicTile/GraphicTile.jsx
+++ services/ui_service/src/components/GraphicTile/GraphicTile.jsx
@@ -5,20 +5,60 @@ import Charts from '../Chart/Chart';
 
 const [chartData, setChartData] = createSignal({});
 
-function transformData(dataset) {
+function transformData(dataset, bestPoint) {
+    let mainColor = "#3BE9E9";
+    let bestColor = "#80ea29";
+    let bestBackgroundColor = "#b7e88f";
+
     return {
         type: "line",
         data: {
             labels: dataset.map((d) => d.profit),
             datasets: [
                 {
+                    label: "The best configuration",
+                    fill: false,
+                    data: bestPoint,
+                    borderColor: bestColor,
+                    pointBackgroundColor: bestBackgroundColor,
+                    pointBorderWidth: 0,
+                    pointRadius: 9,
+                    pointHoverRadius: 10,
+                },
+                {
                     label: "risk(profit)",
                     fill: false,
                     data: dataset.map((d) => (d.risk)),
-                    borderColor: "#3BE9E9"
-                }
-            ]
+                    borderColor: mainColor,
+                    pointBackgroundColor: mainColor,
+                    pointBorderWidth: 3,
+                    pointRadius: 4,
+                    pointHoverRadius: 9,
+                },
+            ],
         },
+        options: {
+            scales: {
+                xAxes: [{
+                    offset: true,
+                    gridLines: {
+                        offsetGridLines: false,
+                        display: true,
+                        borderDash: [6, 2],
+                        tickMarkLength: 5
+                    },
+                }],
+                yAxes: [{
+                    offset: true,
+                    gridLines: {
+                        offsetGridLines: false,
+                        display: true,
+                        borderDash: [6, 2],
+                        tickMarkLength: 5
+                    },
+                }]
+            },
+        }
     }
 }
 
