37224db09fa6500aa7c3c0dcecc3f77da9948a25
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-11-13 19:48:29 +0300

    Remove simple_test.py

--- test/test_ui/test_all.py
+++ test/test_ui/test_all.py
@@ -3,10 +3,10 @@ from selenium.webdriver.chrome.options import Options as ChromiumOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.safari.options import Options as SafariOptions
-# from selenium.webdriver.opera.options import Options as OperaOptions
+from selenium.webdriver.opera.options import Options as OperaOptions
 from selenium.webdriver.chrome.service import Service as СhromiumService
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from webdriver_manager.utils import ChromeType
 from selenium.webdriver.edge.service import Service as EdgeService
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -16,47 +16,61 @@ from webdriver_manager.opera import OperaDriverManager
 from test.test_ui.signup_test import SignUpTests
 from test.test_ui.login_test import LogInTests
 
-
-def run_tests_in_browser(_driver):
-    print(f'Starting tests for {_driver.name}...')
-    SignUpTests(_driver)
-    LogInTests(_driver)
+if __name__ == '__main__':
+    driver_options = ChromiumOptions()
+    driver_options.add_argument("--headless")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(service=СhromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()),
+                              options=driver_options)
+    print('Starting tests for Chromium...')
+    SignUpTests(driver)
+    LogInTests(driver)
     print('All tests passed')
     print('===')
-    _driver.close()
+    driver.close()
 
+    driver_options = EdgeOptions()
+    driver_options.add_argument("--headless")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=driver_options)
+    print('Starting tests for Edge...')
+    SignUpTests(driver)
+    print('All tests passed')
+    print('===')
+    driver.close()
 
-def test_ui():
-    options = [
-        "--headless",
-        "--no-sandbox",
-        "--disable-dev-shm-usage",
-    ]
-    drivers = [
-        # (webdriver.Chrome, СhromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), ChromiumOptions()),
-        # (webdriver.ChromiumEdge, EdgeService(EdgeChromiumDriverManager().install()), EdgeOptions()),
-        (webdriver.Firefox, FirefoxService(GeckoDriverManager().install()), FirefoxOptions()),
-        # (webdriver.Safari, None, SafariOptions()),
-    ]
+    driver_options = FirefoxOptions()
+    driver_options.add_argument("--headless")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=driver_options)
+    print('Starting tests for Firefox...')
+    SignUpTests(driver)
+    print('All tests passed')
+    print('===')
+    driver.close()
 
-    for (web_driver, service, driver_options) in drivers:
-        driver_options.add_argument("--headless")
-        driver_options.add_argument("--no-sandbox")
-        driver_options.add_argument("--disable-dev-shm-usage")
-        if service is None:
-            _driver = web_driver(options=driver_options)
-        else:
-            _driver = web_driver(service=service, options=driver_options)
-        run_tests_in_browser(_driver)
+    driver_options = SafariOptions()
+    driver_options.add_argument("--headless")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Safari(options=driver_options)
+    print('Starting tests for Safari...')
+    SignUpTests(driver)
+    print('All tests passed')
+    print('===')
+    driver.close()
 
-    # driver_options = OperaOptions()
-    # driver_options.add_argument("--headless")
-    # driver_options.add_argument("--no-sandbox")
-    # driver_options.add_argument("--disable-dev-shm-usage")
-    # driver = webdriver.Opera(executable_path=OperaDriverManager().install(), options=driver_options)
-    # print('Starting tests for Opera...')
-    # SignUpTests(driver)
-    # print('All tests passed')
-    # print('===')
-    # driver.close()
+    driver_options = OperaOptions()
+    driver_options.add_argument("--headless")
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Opera(executable_path=OperaDriverManager().install(), options=driver_options)
+    print('Starting tests for Opera...')
+    SignUpTests(driver)
+    print('All tests passed')
+    print('===')
+    driver.close()
 
