4eed55669349690aa1456e3f70346ba75389b98c
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-11-15 13:27:58 +0300

    HISA-77 Update user profile

--- services/ui_service/src/pages/profile/Profile.jsx
+++ services/ui_service/src/pages/profile/Profile.jsx
@@ -1,5 +1,9 @@
 import './Profile.css';
 import SavedConfig from "../../components/SavedConfig/SavedConfig";
+import {createSignal} from "solid-js";
+
+const [username, setUsername] = createSignal("Username");
+const [email, seEmail] = createSignal("username@email.com");
 
 function Profile() {
 
@@ -7,27 +11,34 @@ function Profile() {
         <>
             <div class="container-lg">
                 <div class="row mb-2 ms-2 me-2">
-                    <div class="col-md-8">
-                        <h3 class="title">Your configurations</h3>
-                        <SavedConfig/>
-                    </div>
                     <div class="col-md-4">
-                        <h5 class="title">Change Username or Password</h5>
-                        <div class="auth-field-block">
-                            <label for="username_field">Username</label>
-                            <input type="text" name="username" class="form-control" id="username_field" placeholder="Username"/>
-                        </div>
-                        <div class="auth-field-block">
-                            <label for="floatingPassword">Password</label>
-                            <input type="password" name="password" class="form-control" id="password_field" placeholder="Password"/>
-                        </div>
-                        <div class="auth-field-block">
-                            <label for="floatingPasswordAgain">Password again</label>
-                            <input type="password" class="form-control" id="password_again_field" placeholder="Password again"/>
-                        </div>
-
-                        <button class="auth-button w-100 btn btn-primary" type="submit">Save</button>
+                        <img src="/assets/imgs/user-icon.png" class="user-photo" alt="Icon"/>
                     </div>
+                    <div class="col-md-8 profile-info">
+                        <div class="row">
+                            <h3>
+                                Username
+                            </h3>
+                            <p>
+                                {username()}
+                            </p>
+                        </div>
+                        <div class="row">
+                            <h3>
+                                Email
+                            </h3>
+                            <p>
+                                {email()}
+                            </p>
+                        </div>
+                        {/*<div class="row">*/}
+                        {/*    <button type="button" class="btn btn-outline-secondary" onClick={() => window.location.replace('/auth/change_password?next=/profile')}>Change Password</button>*/}
+                        {/*</div>*/}
+                    </div>
+                </div>
+                <div class="row mb-2 ms-2 me-2">
+                    <h1 class="title">Your configurations</h1>
+                    <SavedConfig/>
                 </div>
             </div>
         </>
