b249348e905bc0952ff0645e12e3ff62b30b9350
Andrey Mozhaev <mozhay2000@gmail.com>
2022-11-16 12:48:20 +0300

    test user_service one user

--- services/user_service/tests/test_pytest.py
+++ services/user_service/tests/test_pytest.py
@@ -1,4 +1,6 @@
 import pytest
+from services.user_service.common.abstract import User
+from services.user_service.db.user_db_api import save_user, get_user_by_login
 
 
 @pytest.mark.parametrize(
@@ -10,3 +12,15 @@ import pytest
 )
 def test_simple(a, b, c):
     assert a + b == c
+
+
+test_data_one_user = [
+    User(login="new_login", password="qwerty"),
+]
+
+
+@pytest.mark.parametrize("user", test_data_one_user)
+def test_add_get_one_user(user):
+    save_user(user)
+    assert get_user_by_login(user.login) == user
+
