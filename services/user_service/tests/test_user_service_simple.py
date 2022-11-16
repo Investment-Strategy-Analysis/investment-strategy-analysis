4d79d0cbdb929fc8f556287e2af99c84cb667be3
Andrey Mozhaev <mozhay2000@gmail.com>
2022-11-16 21:52:29 +0300

    test user_service simple test few users

--- services/user_service/tests/test_user_service_simple.py
+++ services/user_service/tests/test_user_service_simple.py
@@ -13,3 +13,18 @@ def test_add_get_one_user(user):
     save_user(user)
     user_from_db = get_user_by_login(user.login)
     assert user_from_db.login == user.login
+
+
+test_data_few_users = [
+    (User(login="a1", password="qwerty"), User(login="a2", password="qwerty"), User(login="a3", password="qwerty123")),
+]
+
+
+@pytest.mark.parametrize("u1, u2, u3", test_data_few_users)
+def test_add_get_few_users(u1, u2, u3):
+    save_user(u1)
+    save_user(u2)
+    save_user(u3)
+    assert get_user_by_login(u1.login).login == u1.login
+    assert get_user_by_login(u2.login).login == u2.login
+    assert get_user_by_login(u3.login).login == u3.login
