6faafb067b93a3a96f381b2c3edde162eca91c28
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-11-16 23:25:45 +0300

    Try to fix integration tests failure

--- test/test_integration/test_algo/test_api.py
+++ test/test_integration/test_algo/test_api.py
@@ -4,7 +4,7 @@ from test.test_integration.test_algo.consts import SERVER_URL
 
 
 def test_ping():
-    url = f"https://ya.ru"
+    url = f"{SERVER_URL}/ping"
     r = requests.get(url=url)
     assert r.ok
     assert r.content == b'"pong"'
