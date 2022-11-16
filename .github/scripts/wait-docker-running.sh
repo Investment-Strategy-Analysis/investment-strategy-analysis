3b71c82420fb7ed636b0c7dcd2b85eb0353bf14a
Vyacheslav Tamarin <vyacheslav.tamarin@yandex.ru>
2022-11-16 22:40:35 +0300

    Try to fix integration tests failure

---
+++ .github/scripts/wait-docker-running.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+secs=${INPUT_TIMEOUT}
+endTime=$(( $(date +%s) + secs ))
+while [ $(date +%s) -lt $endTime ]; do
+  already_running=$(docker compose ps | grep -c "running")
+  already_running=$((already_running+1))
+  containers_count=$(docker compose ps | wc -l)
+  echo -en "\rWaiting for services... $already_running/$containers_count"
+  if [[ $already_running -eq $containers_count ]]; then
+    echo ""
+    exit 0
+  fi
+  sleep 1
+done
+
+exit 1
