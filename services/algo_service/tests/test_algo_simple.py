a506a43eceeb6d914297baf2b6ce9bd2451317ae
Andrey Mozhaev <mozhay2000@gmail.com>
2022-11-17 16:04:48 +0300

    algo_test simple test get_solutions

---
+++ services/algo_service/tests/test_algo_simple.py
@@ -0,0 +1,22 @@
+from typing import List, Tuple
+from services.algo_service.common.abstract import Restriction, InvestStrategy, Index, Checkbox
+from services.algo_service.algorithm.algorithm_impl import get_solutions as __get_solutions
+
+
+# returns the best solution and all found
+
+
+def get_solutions(restriction: Restriction) -> Tuple[InvestStrategy, List[InvestStrategy]]:
+    return __get_solutions(restriction)
+
+
+def example_test():
+    best, front = get_solutions(Restriction(target_profit=13,
+                                            checkboxes={checkbox.value.id: False for checkbox in Checkbox},
+                                            upper_border={index.value.id: 1 for index in Index},
+                                            lower_border={index.value.id: 0 for index in Index},
+                                            analysis_time=100))
+    print(best)
+    assert best == None
+    print(front)
+    assert front
