4d0920c211b331fd94721d7d8267684b84a14cd9
Andrey Mozhaev <mozhay2000@gmail.com>
2022-11-18 10:57:40 +0300

    algo_test user_db fix branch

--- services/user_service/tests/test_user_service_simple.py
+++ services/user_service/tests/test_user_service_simple.py
@@ -1,6 +1,7 @@
 import pytest
 from services.user_service.common.abstract import User
-from services.user_service.db.user_db_api import save_user, get_user_by_login, delete_user_by_login
+from services.user_service.db.user_db_api import save_user, get_user_by_login
+
 
 test_data_one_user = [
     User(login="new_login", password="qwerty"),
@@ -27,29 +28,3 @@ def test_add_get_few_users(u1, u2, u3):
     assert get_user_by_login(u1.login).login == u1.login
     assert get_user_by_login(u2.login).login == u2.login
     assert get_user_by_login(u3.login).login == u3.login
-    delete_user_by_login(u1.login)
-    delete_user_by_login(u2.login)
-    delete_user_by_login(u3.login)
-
-
-@pytest.mark.parametrize("u1, u2, u3", test_data_few_users)
-def test_add_get_unregistered_users(u1, u2, u3):
-    save_user(u1)
-    save_user(u2)
-    assert get_user_by_login(u1.login).login == u1.login
-    assert get_user_by_login(u2.login).login == u2.login
-    assert get_user_by_login(u3.login).login == None
-    delete_user_by_login(u1.login)
-    delete_user_by_login(u2.login)
-
-
-@pytest.mark.parametrize("u1, u2, u3", test_data_few_users)
-def test_add_same_users(u1, u2, u3):
-    try:
-        save_user(u1)
-        save_user(u1)
-    except Exception as e:
-        delete_user_by_login(u1.login)
-        return
-    assert "Same user saved twice"
-    delete_user_by_login(u1.login)
