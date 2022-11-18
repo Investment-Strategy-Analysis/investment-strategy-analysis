e1e67f85096fb0733a5f78687f656894b9ec9f21
Andrey Mozhaev <mozhay2000@gmail.com>
2022-11-18 11:16:23 +0300

    algo_db fix test_save_history

--- services/algo_service/tests/test_save_load_history.py
+++ services/algo_service/tests/test_save_load_history.py
@@ -5,16 +5,12 @@ from services.algo_service.db.tables import InvestItem
 
 
 test_data_few_users = [
-    InvestItem(),
+    InvestItem(name='t1', country='q', id='id', history=[1]),
 ]
 
 
-@pytest.mark.parametrize("u1, u2, u3", test_data_few_users)
-def test_save_history():
-    a = InvestItem(name='t1', country='q', id='id', history=[1])
-    print("InvestItem crated")
+@pytest.mark.parametrize("a", test_data_few_users)
+def test_save_history(a):
     save_history(a)
-    print("InvestItem saved")
     b = load_history(a)
-    print("InvestItem loaded")
     assert a.id == b.id
