cb03849c02c4f1ac0d80f1cce75391c5fd663467
Andrey Mozhaev <mozhay2000@gmail.com>
2022-11-18 11:21:45 +0300

    algo_db delete test_save_history

--- services/algo_service/tests/test_save_load_history.py
+++ services/algo_service/tests/test_save_load_history.py
@@ -3,14 +3,14 @@ import pytest
 from services.algo_service.db.data_db_api import save_history, load_history
 from services.algo_service.db.tables import InvestItem
 
-
-test_data_few_users = [
-    InvestItem(name='t1', country='q', id='id', history=[1]),
-]
-
-
-@pytest.mark.parametrize("a", test_data_few_users)
-def test_save_history(a):
-    save_history(a)
-    b = load_history(a)
-    assert a.id == b.id
+#
+# test_data_few_users = [
+#     InvestItem(name='t1', country='q', id='id', history=[1]),
+# ]
+#
+#
+# @pytest.mark.parametrize("a", test_data_few_users)
+# def test_save_history(a):
+#     save_history(a)
+#     b = load_history(a)
+#     assert a.id == b.id
